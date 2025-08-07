776cf78d327e5c3ec6bed859a6d82de94fc08085
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2025-08-07 15:34:38 +0100

    Day 3

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,24 +1,76 @@
 #include <windows.h>
+#define internal                                                                                                       \
+    static // if static applied to a function, the function is marked as "internal" to that specific file, and no one
+           // from outside may access it (actually, the locality may extend to more thatn one file, i.e. to the entire
+           // translation unit of the program. We'll speak about the translation units much, much later down the line
+#define local_persist                                                                                                  \
+    static // stays around similar to a 'global_variable', but it's locally scoped. also never use static in the final
+           // code, bad for thread safety and other advanced things. local_persist keyword made so it can be stripped
+           // away from the final code, however handy tool for development, because they allow to quicjly introduce
+           // something to the code without having to worry about passing things around, and this is exactly what we are
+           // doing right now in WM_PAINT: making sure our PatBlt does exactly what we want to it to do.
+#define global_variable                                                                                                \
+    static // once declared, is available in the entirety of the file it's in. Any function can access it, read its
+           // value and modify it
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+// serves to resize or initialise if it doesn't exist a Device Independent Bitmap which is the name Windwos gies to the
+// bitmaps it can display with GDI
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+    // TODO(joey): Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+
+    if (!BitmapDeviceContext)
+    {
+        // TODO(joey): Should we recreate these under certain special circumstances?
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    HBITMAP BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS,
+                  SRCCOPY);
+}
+
+// Event handler, called when something happens to the window
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
     LRESULT Result = 0;
     switch (Message)
     {
-        // When the user changes the size of the window
-    case WM_SIZE: {
-        OutputDebugStringA("WM_SIZE\n");
-    }
-    break;
+    
 
         // When the user deletes our window
     case WM_DESTROY: {
+        Running = false;
         OutputDebugStringA("WM_Destroy\n");
     }
     break;
 
         // When the user clicks on the little X in the top-right corner
     case WM_CLOSE: {
+        Running = false;
+        PostQuitMessage(0);
         OutputDebugStringA("WM_CLOSE\n");
     }
     break;
@@ -30,25 +82,28 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LP
     break;
 
     case WM_PAINT: {
-        PAINTSTRUCT Paint;
+        PAINTSTRUCT Paint = {};
         HDC DeviceContext = BeginPaint(Window, &Paint);
         int X = Paint.rcPaint.left;
         int Y = Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        static DWORD Operation = WHITENESS;
-        if (Operation == WHITENESS)
-        {
-            Operation = BLACKNESS;
-        }
-        else
-        {
-            Operation = WHITENESS;
-        }
-        PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+        Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
         EndPaint(Window, &Paint);
     }
     break;
+
+        // When the user changes the size of the window
+    case WM_SIZE: {
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        int Width = ClientRect.right - ClientRect.left;
+        int Height = ClientRect.bottom - ClientRect.top;
+        Win32ResizeDIBSection(Width, Height);
+    }
+    break;
+            
     default: {
         // Do something in case of any other message
         Result = DefWindowProc(Window, Message, WParam, LParam);
@@ -64,7 +119,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
     WNDCLASSA WindowClass = {};
     // TODO(joey): Check if HREDRAW/VREDRAW/OWNDC still matter
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     // WindowClass.hIcon
     WindowClass.lpszClassName = "HandmadeHeroClass";
@@ -75,20 +130,16 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
         if (Window)
         {
 
-            /*MSG Message;
-            while (GetMessage(&Message, 0, 0, 0))
-            {
-
-            }*/
-            for (;;)
+            Running = true;
+            while (Running)
             {
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0); // wait for a message
                 if (MessageResult > 0)
                 {
                     // 0 is the WM_QUIT message, -1 is invalid window handle
-                    TranslateMessage(&Message);
-                    DispatchMessageA(&Message);
+                    TranslateMessage(&Message); // process message
+                    DispatchMessageA(&Message); // send to callback
                 }
                 else
                 {
